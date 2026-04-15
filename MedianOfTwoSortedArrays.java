f2f07a8d9c1c92280132c107aaa1adf68843a85a
Vaaruni S <vaarunis20@gmail.com>
2026-04-15 22:07:58 +0530

    MedianOfTwoSortedArrays.java

---
+++ MedianOfTwoSortedArrays.java
@@ -0,0 +1,39 @@
+class Solution {
+    public double findMedianSortedArrays(int[] a, int[] b) {
+        if (a.length > b.length) {
+            return findMedianSortedArrays(b, a); // ensure a is smaller
+        }
+
+        int n = a.length, m = b.length;
+        int low = 0, high = n;
+
+        while (low <= high) {
+            int cut1 = (low + high) / 2;
+            int cut2 = (n + m + 1) / 2 - cut1;
+
+            int l1 = (cut1 == 0) ? Integer.MIN_VALUE : a[cut1 - 1];
+            int l2 = (cut2 == 0) ? Integer.MIN_VALUE : b[cut2 - 1];
+
+            int r1 = (cut1 == n) ? Integer.MAX_VALUE : a[cut1];
+            int r2 = (cut2 == m) ? Integer.MAX_VALUE : b[cut2];
+
+            // correct partition
+            if (l1 <= r2 && l2 <= r1) {
+                if ((n + m) % 2 == 0) {
+                    return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+                } else {
+                    return Math.max(l1, l2);
+                }
+            }
+            // move left
+            else if (l1 > r2) {
+                high = cut1 - 1;
+            }
+            // move right
+            else {
+                low = cut1 + 1;
+            }
+        }
+        return 0.0;
+    }
+}
