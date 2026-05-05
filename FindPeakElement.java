bc57137ee47c809b5d9e0f9d1c7be72c3465da3e
Vaaruni S <vaarunis20@gmail.com>
2026-05-05 21:52:24 +0530

    FindPeakElement.java

---
+++ FindPeakElement.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int left = 0, right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] < nums[mid + 1]) {
+                left = mid + 1;   // go right
+            } else {
+                right = mid;      // go left
+            }
+        }
+
+        return left; // or right (both same)
+    }
+}
