65b67729e0b7e050caf32deda7fa4e785ac9a095
Vaaruni S <vaarunis20@gmail.com>
2026-05-29 21:46:39 +0530

    SearchIn2DMatrix.java

---
+++ SearchIn2DMatrix.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int low = 0;
+        int high = rows * cols - 1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            int r = mid / cols;
+            int c = mid % cols;
+
+            if (matrix[r][c] == target) {
+                return true;
+            }
+
+            else if (matrix[r][c] < target) {
+                low = mid + 1;
+            }
+
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return false;
+    }
+}
